f3e3290ada22e2b355d5eeb1b43c8761f285eee9
afusco <anthony.fusco42@gmail.com>
2018-01-08 10:18:10 +0100

    Initial commit

---
+++ app/src/main/java/com/ihm/unice/incident_reporting/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ihm.unice.incident_reporting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
