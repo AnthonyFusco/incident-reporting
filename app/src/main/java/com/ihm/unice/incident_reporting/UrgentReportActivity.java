0736fea70a72c6f5abbc045a0f094702fc8fff79
afusco <anthony.fusco42@gmail.com>
2018-01-29 11:56:55 +0100

    button

--- app/src/main/java/com/ihm/unice/incident_reporting/UrgentReportActivity.java
+++ app/src/main/java/com/ihm/unice/incident_reporting/UrgentReportActivity.java
@@ -15,45 +15,19 @@ import com.ihm.unice.incident_reporting.models.UrgentType;
 import com.ihm.unice.incident_reporting.models.User;
 import com.ihm.unice.incident_reporting.repositories.IncidentsRepository;
 import com.ihm.unice.incident_reporting.repositories.RepositoryFactory;
-import com.ihm.unice.incident_reporting.repositories.RepositoryIncidentsBase;
 
 import java.util.Date;
 
 public class UrgentReportActivity extends MenuBaseActivity {
-    private IncidentsRepository repository = RepositoryFactory.createRepository();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.urgent_report);
-
-        Spinner spinner = findViewById(R.id.urgentSpinner);
-
-        ArrayAdapter<UrgentType> adapter =
-                new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,
-                        repository.getUrgentTypes());
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        Spinner whoCall = findViewById(R.id.who_call);
-        ArrayAdapter<CharSequence> whoCallAdapter = ArrayAdapter.createFromResource(this,
-                R.array.who_call, android.R.layout.simple_spinner_item);
-        whoCallAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        whoCall.setAdapter(whoCallAdapter);
-
-        Spinner whoSignal = findViewById(R.id.who);
-        ArrayAdapter<CharSequence> whoSignalAdapter = ArrayAdapter.createFromResource(this,
-                R.array.who, android.R.layout.simple_spinner_item);
-        whoSignalAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        whoSignal.setAdapter(whoSignalAdapter);
     }
 
-    public void onClickSignalize(View view) {
+    public void baseOnClick() {
         Intent intent = new Intent(this, ConfirmIncidentActivity.class);
-        MainActivity.addIncident(new Incident(new User("Danièle"), new Date(),
-                ((Spinner) findViewById(R.id.urgentSpinner)).getSelectedItem().toString()));
-
         Intent call = new Intent(Intent.ACTION_CALL);
 
         call.setData(Uri.parse("tel:" + "0613548760"));
@@ -69,4 +43,30 @@ public class UrgentReportActivity extends MenuBaseActivity {
         startActivity(intent);
         finish();
     }
+
+    public void onClickFire(View view) {
+        MainActivity.addIncident(new Incident(new User("Danièle"), new Date(), "Incendie"));
+        baseOnClick();
+    }
+
+    public void onClickInondation(View view) {
+        MainActivity.addIncident(new Incident(new User("Danièle"), new Date(),"Inondation"));
+        baseOnClick();
+    }
+
+    public void onClickGaz(View view) {
+        MainActivity.addIncident(new Incident(new User("Danièle"), new Date(),"Gaz"));
+        baseOnClick();
+    }
+
+    public void onClickMalaise(View view) {
+        MainActivity.addIncident(new Incident(new User("Danièle"), new Date(),"Malaise"));
+        baseOnClick();
+    }
+
+    public void onClickVol(View view) {
+        MainActivity.addIncident(new Incident(new User("Danièle"), new Date(),"Vol"));
+        baseOnClick();
+    }
+
 }
