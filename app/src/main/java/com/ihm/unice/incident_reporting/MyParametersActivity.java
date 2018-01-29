9c6e236654f4ba921e5c8af36bb60de95e683179
afusco <anthony.fusco42@gmail.com>
2018-01-29 09:10:00 +0100

    color

---
+++ app/src/main/java/com/ihm/unice/incident_reporting/MyParametersActivity.java
@@ -0,0 +1,54 @@
+package com.ihm.unice.incident_reporting;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.util.List;
+
+
+public class MyParametersActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preferences_header, target);
+    }
+
+    /**
+     * This fragment shows the preferences for the first header.
+     */
+    public static class Prefs1Fragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.normal_preferences);
+        }
+    }
+
+    /**
+     * This fragment shows the preferences for the second header.
+     */
+    public static class Prefs2Fragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Can retrieve arguments from headers XML.
+            Log.i("args", "Arguments: " + getArguments());
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.urgent_preferences);
+        }
+    }
+
+}
